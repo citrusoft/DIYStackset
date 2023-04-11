bc1c5c978856f3b450248748f0d0f9fb8944b904
Tommy Hunt <tahv@pge.com>
2023-04-11 08:50:23 -0700

    main: PASS deploying role across dir of accounts.

--- stacksets.py
+++ stacksets.py
@@ -1,11 +1,43 @@
+import argparse
+import glob
+import os
 import subprocess
+import re
 
 import boto3
+
+# RESOURCE_PATH = "/Users/TAHV/tmp/resources"
+RESOURCE_PATH = "/Users/TAHV/git/aws-lz-iam/cscoe-iam-kiosk/test/"
 
 def init():
   subprocess.run(f"terraform init", check=True, shell=True)
 
-def get_accounts():
+def get_paths_from_dir() -> list:
+
+  # Extract all the list of items recursively
+  files = glob.glob(RESOURCE_PATH + '**/service-accounts/*', recursive=True)
+ 
+  # Filter only files
+  files = [f for f in files if os.path.isfile(f)]
+
+  for filename in files:
+    print(filename)
+  return files
+
+
+def get_accounts_from_paths() -> list:
+
+  paths = get_paths_from_dir()
+          
+  # for path in paths:
+  #   print(re.findall("[0-9]{12}", path))
+    # print(path.split("/"))
+  return [
+        re.findall("[0-9]{12}", path)[0]
+        for path in paths
+  ]
+
+def get_accounts_from_orgs():
   organizations = boto3.client('organizations')
   paginator = organizations.get_paginator("list_accounts")
 
@@ -31,17 +63,33 @@ def plan(account):
 def apply(account):
   subprocess.run(f"terraform apply -var target_account_id={account} -auto-approve", check=True, shell=True)
 
+def destroy(account):
+  subprocess.run(f"terraform destroy -var target_account_id={account} -auto-approve", check=True, shell=True)
+
 def run():
   init()
-  for account in get_accounts():
-    if account != "424304752381":
-      continue
+  for account in get_accounts_from_paths():
+    # if account != "424304752381":
+    #   continue
     if not workspace_exists(account):
       create_workspace(account)
     switch_to_workspace(account)
-    plan(account)
-    apply(account)
+    if args.plan:
+      plan(account)
+    if args.apply:
+      apply(account)
+    if args.destroy:
+      destroy(account)
+
+parser = argparse.ArgumentParser(
+                    prog='terrastacks',
+                    description='stackset-esque functionality for your Terraform code.',
+                    epilog='This will make deploying common infrastructure across AWS accts significantly simpler.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-a', '--apply', action='store_true')
+group.add_argument('-d', '--destroy', action='store_true')
+parser.add_argument('-p', '--plan', action='store_true')
+args = parser.parse_args()
 
 if __name__ == "__main__":
   run()
-
