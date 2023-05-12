7de807daec754d6362a360424853b946e365965b
Tommy Hunt <tahv@pge.com>
2023-05-12 10:22:48 -0700

    tfe-workspaces: regorganization of folders.

--- iam-kiosk/pipeline/outputs.tf
+++ iam-kiosk/pipeline/outputs.tf
@@ -2,6 +2,14 @@ output "role_specs" {
   value       = local.role_specs
 }
 
+output "federated_roles" {
+  value       = module.federated_roles
+}
+
 output "user_specs" {
   value       = local.user_specs
+}
+
+output "service_accounts" {
+  value       = module.service_accounts
 }
